77770d2f6abb1662acaede95f34f6f1402569b47
Victor Payno <vpayno@users.noreply.github.com>
2024-05-08 20:51:23 -0700

    hello-world: complete tutorial

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -1,3 +1,16 @@
+use cursive::views::{Dialog, TextView};
+
 fn main() {
-    println!("Hello, world!");
+    // Creates the cursive root
+    let mut siv = cursive::default();
+
+    // Creates a dialog with a single "Quit" button.
+    siv.add_layer(
+        Dialog::around(TextView::new("Hello World!"))
+            .title("Cursive")
+            .button("Quit", |s| s.quit()),
+    );
+
+    // Starts the event loop.
+    siv.run();
 }
