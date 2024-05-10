837e1daf7dd7bfed929dcf87681cb83d0e107787
Victor Payno <vpayno@users.noreply.github.com>
2024-05-09 20:53:22 -0700

    tutorial_one: finish tutorial 2 of 3

--- tutorial_two/src/main.rs
+++ tutorial_two/src/main.rs
@@ -1,12 +1,38 @@
-use cursive::views::TextView;
+use cursive::views::Dialog;
+use cursive::Cursive;
 
 fn main() {
     let mut siv = cursive::default();
 
     siv.add_global_callback('q', |s| s.quit());
 
-    // no dialog/border, has drop shadow - label with shadow
-    siv.add_layer(TextView::new("Hello cursive! Press <q> to quit."));
+    // dialog with thin border with shadow
+    siv.add_layer(
+        Dialog::text("Press the button!")
+            .title("Frist dialog")
+            .button("Next", show_next),
+    );
 
     siv.run();
+}
+
+fn show_next(s: &mut Cursive) {
+    s.pop_layer();
+    s.add_layer(
+        Dialog::text("Did you do the thing?")
+            .title("Question 1")
+            .button("Yes!", |s| show_answer(s, "I knew it! Well done!"))
+            .button("No!", |s| show_answer(s, "I knew you couldn't be trusted!"))
+            .button("Uh?", |s| s.add_layer(Dialog::info("Try again!")))
+            .button("Nevermind", |s| s.quit()),
+    );
+}
+
+fn show_answer(s: &mut Cursive, msg: &str) {
+    s.pop_layer();
+    s.add_layer(
+        Dialog::text(msg)
+            .title("Results")
+            .button("Finish", |s| s.quit()),
+    );
 }
